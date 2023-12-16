51a3af880b9d1ee37c3facc7d8eb8be8e2f4539a
yureutaejin <85734054+yureutaejin@users.noreply.github.com>
2023-12-16 16:13:40 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "yureutae",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Researcher",
+    bio: "I'm interested in DevOps, Cloud, Data Engineering now",
+    email: "jyjin3@naver.com",
+    linkedin: "https://www.linkedin.com/in/yuntae-jin-4bab97201/",
+    github: "https://github.com/yureutaejin",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `yureutae-log`,
+      href: "https://github.com/yureutaejin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "yureutae-log",
+    description: "welcome to yureutae-log!",
   },
 
   // CONFIG configration (required)
@@ -47,7 +47,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
