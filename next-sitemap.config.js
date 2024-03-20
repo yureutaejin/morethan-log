c20d8bcb06de8cccf866451d58ac553f6a452086
Yuntae Jin <jyjin3@me.com>
2024-03-20 12:21:55 +0900

    build: chore

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,17 +6,12 @@ module.exports = {
   sitemapSize: 7000,
   generateIndexSitemap: false,
   changefreq: 'daily',
-  exclude: ['/server-sitemap-index.xml'],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
       },
-    ],
-    host: CONFIG.link,
-    additionalSitemaps: [
-      `${CONFIG.link}/server-sitemap-index.xml`,
     ],
   },
 }
